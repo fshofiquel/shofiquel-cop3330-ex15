8e3d85847207e1aad0a389e149f32eaa267943aa
fshofiquel <fshofique@gmail.com>
2021-06-06 23:06:59 -0400

    update

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -3,10 +3,21 @@ package base;/*
  *  Copyright 2021 Fazlur Shofiquel
  */
 
+import java.util.Scanner;
+
 public class App
 {
     public static void main(String[] args)
     {
-        
+        String password;
+        Scanner passIn = new Scanner(System.in);
+
+        System.out.print("What is the password? ");
+        password = passIn.nextLine();
+
+        if (password.equals("abc$123")) System.out.print("Welcome!");
+
+        else System.out.print("I don't know you.");
+
     }
 }
